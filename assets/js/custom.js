7bc7c5180ae7b0bac913f44a207c567ea4388ba7
fitrianabirthday <97207447+fitrianabirthday@users.noreply.github.com>
2022-01-06 10:17:44 +0700

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,48 @@
+$(document).ready(function(){
+  
+  setTimeout(function() {
+    $('#loader-display').hide();
+    $('.page1').slideDown();
+  }, 2000);
 
+
+
+  $("#btn-slide").click(function(){
+    $(".page1").slideUp();
+    $(".page2").slideDown();
+  });
+
+
+  $('input[type="text"]').keyup(function() {
+        if($(this).val() != '') {
+           $('#btn-name').prop('disabled', false);
+        };
+     });
+
+
+  $("#btn-name").click(function(){
+    $(".page2").slideUp();
+    $(".page3").slideDown();
+    $('#name-display').text($('input[type="text"]').val());
+  });
+
+
+  $("#btn-slide2").click(function(){
+    $('.page3').slideUp();
+    $('.page4').slideDown();
+  });
+
+  $("#btn-iya").click(function(){
+    $(".page4").slideUp();
+    $(".page5").slideDown();
+  });
+
+  $("#btn-lanjut").click(function(){
+    $(".page4").slideUp();
+    $(".page5").slideDown();
+  });
+
+  $("#btn-play").click(function(){
+    $("#music-play").get(0).play();
+  });
+});
